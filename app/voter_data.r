618642f64fe17850afc0c976e64f3a7ad4169a13
Kevin Mendez <mendekev@uw.edu>
2019-12-03 14:50:22 -0800

    updated with README due to merge error

--- app/voter_data.r
+++ app/voter_data.r
@@ -5,19 +5,27 @@ library(ggplot2)
 # Data manipulation goes here
 
 # Load datasets in only once.
-# TODO: Manipulate national-level data
-# natl_voter_reg <- read.csv("data/national_voter_registration.csv", stringsAsFactors = FALSE)
+# National-level data
+natl_voter_reg <- read.csv("data/national_voter_registration.csv", stringsAsFactors = FALSE)
+View(natl_voter_reg)
+
+x <-natl_voter_reg$STATE
+
+natl_voter_reg<- as.data.frame(t(natl_voter_reg[,-1]))
+colnames(natl_voter_reg) <- x
+###creating vectors to look at voter participation
+#natl_reg <- natl_voter_reg$Registered / natl_voter_reg$Total.population
+#voter_turnout <- natl_voter_reg$Voted / natl_voter_reg$Registered
+
+# Washington State data
 WACountyReg <- read.csv("data/wa_reg_voters_by_age.csv", header = TRUE,
                         stringsAsFactors = FALSE)
-
+View(WACountyReg)
 n <- WACountyReg$County
 
 WACountyReg <- as.data.frame(t(WACountyReg[,-1]))
 colnames(WACountyReg) <- n
 WACountyReg$Age.17.24 <- factor(row.names(WACountyReg))
-
-# View(WACountyReg)
-
 
 
 WACountyTotal <- read.csv("data/wa_pop_by_age.csv", header = TRUE,
@@ -28,6 +36,3 @@ m <- WACountyTotal$County
 WACountyTotal <- as.data.frame(t(WACountyTotal[,-1]))
 colnames(WACountyTotal) <- m
 WACountyTotal$Age.17.24 <- factor(row.names(WACountyTotal))
-
-  
-  
--- app/voter_reg_ui.R
+++ app/voter_reg_ui.R
@@ -14,20 +14,31 @@ ui <- navbarPage("US Voter Registration",
            htmlOutput("methodDescription")),
   
   navbarMenu("Results",
-             # National-Level Analysis
-             tabPanel("National Data",
-                      htmlOutput("nationalDescription")),
-             
-             # Washington State Analysis
-             tabPanel("Washington State Data",
+            #National-Level Analysis
+            tabPanel("National Data",
+                      #htmlOutput("nationalDescription")
                       sidebarLayout(
+                        sidebarPanel(
+                          selectInput("STATE",
+                                      label = h3("Select State"),
+                                      choices = colnames(natl_voter_reg),
+                                      selected = "UNITED STATES")
+                        ),
+                        mainPanel(
+                          tabPanel("National Data")
+                        )
+                      )
+            ),
+             
+            # Washington State Analysis
+            tabPanel("Washington State Data",
+                     sidebarLayout(
                         sidebarPanel(
                           selectInput("County", 
                                       label=h3("Select County"),
                                       choices = colnames(WACountyReg),
                                       selected = "TOTAL")
                         ),
-                        
                         # Results are displayed here
                         # TODO: Fix the issue with spacing that occurs here
                         mainPanel(
@@ -35,19 +46,12 @@ ui <- navbarPage("US Voter Registration",
                                    plotOutput("voterPlot"),
                                    plotOutput("overlayPlot"),
                                    htmlOutput("waDescription"))
-                          
                         )
-                        
-                        
-                      )
-             
-           )
-          ),
-  
-  tabPanel("Conclusions",
-           htmlOutput("conclusionDescription")),
-  
-  tabPanel("Other Info",
-           htmlOutput("otherDescription"))
-  
+                     )
+            )
+  ),
+            tabPanel("Conclusions",
+                     htmlOutput("conclusionDescription")),
+            tabPanel("Other Info",
+                     htmlOutput("otherDescription"))
 )
